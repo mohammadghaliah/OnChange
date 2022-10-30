5238f0d91c31169d01ca84aa6f1a0751d9f76d23
mohammadghaliah <94458537+mohammadghaliah@users.noreply.github.com>
2022-10-30 15:47:30 +0200

    Initial commit

---
+++ src/app/child/child.component.ts
@@ -0,0 +1,27 @@
+import {
+  Component,
+  OnInit,
+  OnChanges,
+  SimpleChanges,
+  Input,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  templateUrl: './child.component.html',
+  styleUrls: ['./child.component.css'],
+})
+export class ChildComponent implements OnInit {
+  @Input('number') number: any;
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    //it works whenever the value given to subcription parameter in the parent changes
+    if (changes.number) {
+      console.log('s', this.number);
+    }
+  }
+}
